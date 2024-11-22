cca1a221c466ffeba70b6f89772d51f7302e0601
Dinesh <dineshdeena7040@gmail.com>
2024-11-22 15:08:43 +0530

    bote3 script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,15 @@
 # Define the virtual environment directory
 VENV_DIR="venv"
 
+# Check if the python3.8-venv package is installed
+if ! dpkg -l | grep -q python3-venv; then
+    echo "python3-venv package not found. Installing it..."
+    sudo apt update
+    sudo apt install -y python3-venv
+else
+    echo "python3-venv package is already installed."
+fi
+
 # Create a virtual environment
 if [ ! -d "$VENV_DIR" ]; then
     python3 -m venv "$VENV_DIR"
